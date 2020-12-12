381ec48ee031f9023ebfe8c1d4cd2757db669137
robertokbr <robertojuniordev@gmail.com>
2020-12-12 09:41:08 -0300

    Add class EvilGitMerge

---
+++ src/util/EvilGitMerge.ts
@@ -0,0 +1,11 @@
+import { window, Position } from 'vscode';
+
+export default class EvilGitMerge {
+  private HEADTEXT = '<<<<<<<< HEAD';
+  private BODYTEXT = '========';
+  private FOOTERTEXT = '>>>>>>> master';
+
+  public replace() {
+    
+  };
+};
